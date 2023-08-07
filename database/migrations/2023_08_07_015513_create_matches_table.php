ed80b7892d1ac42d6188f6be7fa8bbfdef06435d
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2023-08-07 09:11:22 +0700

    initialisasi untuk migrasi tabel

---
+++ database/migrations/2023_08_07_015513_create_matches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->string('id', 50)->primary();
+            $table->string('home_team_id', 50);
+            $table->string('away_team_id', 50);
+            $table->integer('home_score');
+            $table->integer('away_score');
+
+            $table->foreign('home_team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('away_team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
---
+++ database/migrations/2023_08_07_015523_create_standings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('standings', function (Blueprint $table) {
+            $table->string('id', 50)->primary();
+            $table->string('team_id', 50);
+            $table->integer('total_matches');
+            $table->integer('goals_scored');
+            $table->integer('goals_against');
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('standings');
+    }
+};
